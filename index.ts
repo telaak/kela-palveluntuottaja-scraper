40f6846db26b1f3cf5312b92315bc2adb25808eb
Teemu L <teemulaaks@gmail.com>
2023-11-18 21:00:00 +0200

    add cron and environmental variables

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { KelaParser } from "./parser";
 import { Kunta, Lakiperuste, Kuntoutusmuoto, Kieli } from "./types/enums";
-import { writeFile, readFile } from "fs/promises";
+import { writeFile } from "fs/promises";
+import { CronJob } from "cron";
 
 const kunnat = process.env.KUNNAT
   ? process.env.KUNNAT.split(",")
@@ -10,6 +11,26 @@ const kunnat = process.env.KUNNAT
 const toBeParsed = Object.entries(Kunta).filter(([kuntaName, kuntaNumber]) => {
   return kunnat.includes(kuntaNumber) || kunnat.includes(kuntaName);
 });
+
+if (process.env.PARSE_ON_BOOT === "true") {
+  try {
+    iterate();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+if (process.env.CRON) {
+  console.log(`cronjob scheduled for ${process.env.CRON}`);
+  const job = CronJob.from({
+    cronTime: process.env.CRON,
+    onTick: function () {
+      iterate();
+    },
+    start: true,
+    timeZone: "Europe/Helsinki",
+  });
+}
 
 async function iterate() {
   for (const [kuntaName, kuntaNumber] of toBeParsed) {
@@ -25,28 +46,20 @@ async function iterate() {
       const therapist = await parser.getTherapistInfo(i, i % 20 === 0);
       console.log(therapist);
 
-      if (process.env.SAVE_JSON) {
-        await writeFile(
+      if (process.env.SAVE_JSON === "true") {
+        writeFile(
           `./out/${kuntaName}-${therapist.name}.json`,
           JSON.stringify(therapist, null, 2)
         ).catch(console.error);
       }
 
       if (process.env.API_URL) {
-        await axios
-          .post(process.env.API_URL, {
-            therapist,
-          })
-          .catch(console.error);
+        axios.post(process.env.API_URL, therapist).catch(console.error);
       }
     }
   }
-}
 
-if (process.env.PARSE_ON_BOOT) {
-  try {
-    iterate();
-  } catch (error) {
-    console.error(error);
+  if (!process.env.CRON) {
+    process.exit();
   }
 }
