42c3444a43991f60d99b9ed4ed7f80218cf9a109
Teemu L <teemulaaks@gmail.com>
2023-11-18 16:57:00 +0200

    remove "backend"

--- prisma/migrations/20231116194231_fix_uppercase/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `phoneNUmbers` on the `Therapist` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Therapist" DROP COLUMN "phoneNUmbers",
-ADD COLUMN     "phoneNumbers" TEXT[];
